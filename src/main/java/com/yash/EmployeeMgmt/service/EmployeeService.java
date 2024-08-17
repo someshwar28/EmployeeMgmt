1c3d338b6724c8debac811178cf7052f1c004daa
someshwar <someshwar.kharad28@gmail.com>
2024-08-17 12:21:20 +0530

    added few logs in employeeService class

--- src/main/java/com/yash/EmployeeMgmt/service/EmployeeService.java
+++ src/main/java/com/yash/EmployeeMgmt/service/EmployeeService.java
@@ -2,6 +2,8 @@ package com.yash.EmployeeMgmt.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,8 @@ public class EmployeeService {
 	String message="Employee is not exist with this id ";
 
 	public Employee saveEmployeeObject(Employee employee) {
+
+		logger.info("we are in service class saveEmployeeObject method");
 		return employeeRepository.save(employee);
 
 	}
